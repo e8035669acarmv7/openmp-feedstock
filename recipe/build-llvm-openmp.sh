c744cbb9a4d0728355d1c7fb3c4e0ebe67332d2c
Yu-Ren Zhang <e8035669@gmail.com>
2023-05-14 20:30:37 +0800

    armv7l

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+env
+CFLAGS="$(echo ${CFLAGS} | sed 's/-march=armv7/-march=armv7-a/g')"
+CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-march=armv7/-march=armv7-a/g')"
+
 # using subproject sources has been effectively broken in LLVM 14,
 # so we use the entire project, but make sure we don't pick up
 # anything in-tree other than openmp & the shared cmake folder
@@ -57,7 +61,7 @@ rm -f $PREFIX/lib/libgomp$SHLIB_EXT
 mkdir -p $PREFIX/lib/clang/$PKG_VERSION/include
 # Standalone libomp build doesn't put omp.h in clang's default search path
 cp $PREFIX/include/omp.h $PREFIX/lib/clang/$PKG_VERSION/include
-if [[ "$target_platform" == linux-* ]]; then
+# if [[ "$target_platform" == linux-* ]]; then
   # move libarcher.so so that it doesn't interfere
-  mv $PREFIX/lib/libarcher.so $PREFIX/lib/libarcher.so.bak
-fi
+#   mv $PREFIX/lib/libarcher.so $PREFIX/lib/libarcher.so.bak
+# fi
